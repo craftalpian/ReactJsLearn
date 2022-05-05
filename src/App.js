68268bf6d49856e5b5837d62f3f82d519cbf8404
craftalpian <alfiananandaputra@gmail.com>
2022-05-05 19:10:23 +0700

    Done NavigationBar

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import './App.css';
+import {ReactComponent as SearchIcon} from './assets/images/SearchIcon.svg';
 
 function NavigationBar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-          </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></hr></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+    <section className="container mt-3">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">M Group</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Properties</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Agents</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">Contacts</a>
+              </li>
+            </ul>
+          </div>
+          <SearchIcon width={23} height={23}></SearchIcon>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </section>
   )
 }
 
